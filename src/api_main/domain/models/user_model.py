7ea6683d0cee58b4c903ceb9f3d003f03dfe8dac
Klayton COD.ias <65176457+Ton-Chyod-s@users.noreply.github.com>
2025-05-15 13:13:51 -0400

    feat: adicionar colunas cpf_cnpj, email e telefone à tabela users

--- src/api_main/domain/models/user_model.py
+++ src/api_main/domain/models/user_model.py
@@ -7,3 +7,18 @@ class User(Base):
     __tablename__ = "users"
     id = Column(Integer, primary_key=True)
     name = Column(String)
+    cpf_cnpj = Column(String)
+    email = Column(String)
+    telefone = Column(String)
+
+    __doc__ = "Modelo de Usuário"
+
+    id.__doc__ = "ID do usuário"
+    name.__doc__ = "Nome do usuário"
+    cpf_cnpj.__doc__ = "CPF ou CNPJ do usuário"
+    email.__doc__ = "Email do usuário"
+    telefone.__doc__ = "Telefone do usuário"
+
+    def __repr__(self):
+        return f"User(id={self.id}, name={self.name}, cpf_cnpj={self.cpf_cnpj}, email={self.email}, telefone={self.telefone})"
+    
