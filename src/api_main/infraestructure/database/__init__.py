695316707f86583a042635ccd8c17040b6d00973
Klayton COD.ias <65176457+Ton-Chyod-s@users.noreply.github.com>
2025-05-15 17:13:54 -0400

    feat: adicionar configuração do banco de dados e inicialização, incluindo variáveis de ambiente

---
+++ src/api_main/infraestructure/database/__init__.py
@@ -0,0 +1,5 @@
+from .engine import engine, get_db
+from .base import init_db
+
+
+__all__ = ["engine", "get_db", "init_db"]
--- src/api_main/main.py
+++ src/api_main/main.py
@@ -1,9 +1,21 @@
+from colorama import init
 from flask import Flask
+from dotenv import load_dotenv
+from .infraestructure.database import init_db, engine
 from src.api_main.config import Config
+import os
+
+load_dotenv()
+
+secret_key = os.getenv("SECRET_KEY")
 
 app = Flask(__name__)
+app.secret_key = secret_key
+
 app.config.from_object(Config)
 
+init_db(engine) 
 
 if __name__ == "__main__":
     app.run(host=app.config['HOST'], port=app.config['PORT'], debug=app.config['DEBUG'])
+    
