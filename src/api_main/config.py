77af73cc363913521e1580e81694ca0e7e8e3611
Klayton COD.ias <65176457+Ton-Chyod-s@users.noreply.github.com>
2025-05-15 22:37:53 -0400

    refactor: remover linha em branco desnecessária no arquivo de configuração

--- src/api_main/config.py
+++ src/api_main/config.py
@@ -4,7 +4,6 @@ class Config:
     DEBUG = True
     HOST = '0.0.0.0'
     PORT = 5002
-    
     JWT_SECRET = os.getenv('JWT_SECRET')
     JWT_ALGORITHM = 'HS256'
     JWT_EXP_DELTA_SECONDS = 3600  
