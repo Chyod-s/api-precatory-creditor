72d463c383c1e34e279a973544872cadd339f352
Klayton COD.ias <65176457+Ton-Chyod-s@users.noreply.github.com>
2025-05-16 10:28:02 -0400

    feat: adicionar validações e codificação de arquivo na criação de documento pessoal

--- src/api_main/interface/http/controllers/personal_document_controller.py
+++ src/api_main/interface/http/controllers/personal_document_controller.py
@@ -1,4 +1,6 @@
+import base64
 from flask import jsonify, request
+from werkzeug.datastructures import FileStorage
 from flask_jwt_extended import get_jwt_identity
 from src.api_main.domain.models.creditor_model import Creditor
 from src.api_main.usecases.personal_document.personal_document_user_usecase import PersonalDocumentUserUseCase
@@ -15,11 +17,18 @@ def create_personal_document():
         return jsonify({"error": "creditor not found!"}), 404
 
     try:
+        file = request.files.get('arquivo_url')
+    
+        if not isinstance(file, FileStorage):
+            return jsonify({"error": "File not found or invalid format"}), 400
+
+        encoded_string = base64.b64encode(file.read()).decode('utf-8')
+        
         use_case = PersonalDocumentUserUseCase(db)
         result = use_case.execute(
             credor_id=creditor.id,
             tipo=data.get('tipo'),
-            arquivo_url=data.get('arquivo_url'),
+            arquivo_url=encoded_string,
             enviado_em=data.get('enviado_em')
         )
 
@@ -29,3 +38,4 @@ def create_personal_document():
 
     except ValueError as e:
         return {"error": str(e)}, 400
+    
