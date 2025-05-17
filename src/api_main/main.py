97c6c20130372b9191d3b74eed1edcb77ef154c8
Klayton COD.ias <65176457+Ton-Chyod-s@users.noreply.github.com>
2025-05-17 12:47:27 -0400

    feat: adicionar renderização de template para a rota home e configurar pastas estáticas e de templates

--- src/api_main/main.py
+++ src/api_main/main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from dotenv import load_dotenv
 from flask_jwt_extended import JWTManager
 from src.api_main.infraestructure.database import init_db, engine
@@ -11,18 +11,24 @@ load_dotenv()
 
 secret_key = os.getenv("SECRET_KEY")
 
-app = Flask(__name__)
-app.secret_key = secret_key
+app = Flask(__name__,
+            static_folder='static',
+            template_folder='templates')
 
+app.secret_key = secret_key
 app.config.from_object(Config)
+
+api.add_namespace(user_ns)
 
 api.init_app(app)
 
 jwt = JWTManager(app)
 
-api.add_namespace(user_ns)
+@app.route('/home')
+def home():
+    return render_template('layouts/base.html')
 
-init_db(engine) 
+init_db(engine)
 
 if __name__ == "__main__":
     app.run(host=app.config['HOST'], port=app.config['PORT'], debug=app.config['DEBUG'])
