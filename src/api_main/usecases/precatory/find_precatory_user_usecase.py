040869d510518763ed586a6532ef45cc911d5e9d
Klayton COD.ias <65176457+Ton-Chyod-s@users.noreply.github.com>
2025-05-17 09:25:58 -0400

    fix: corrigir tratamento de data de publicação e nome da função de busca de precatórios

--- src/api_main/usecases/precatory/find_precatory_user_usecase.py
+++ src/api_main/usecases/precatory/find_precatory_user_usecase.py
@@ -7,18 +7,21 @@ class FindPrecatoryUserUseCase:
         self.db = db
 
     def execute(self, credor_id: int, numero_precatorio: str, valor_nominal: float, foro: str, data_publicacao: str):
+        credor_id = int(credor_id)
+   
         if not isinstance(credor_id, int):
             raise CustomAPIException("ID do credor deve ser um número inteiro.", 422)
 
-        data_publicacao_date = datetime.strptime(data_publicacao, '%d/%m/%Y').date()
+        if data_publicacao is not None:
+                data_publicacao_date = datetime.strptime(data_publicacao, '%d/%m/%Y').date()
 
-        precatory = Precatory.get_all_precatory(
+        precatory = Precatory.get_all_precatories(
             db=self.db,
             credor_id=credor_id,
             numero_precatorio=numero_precatorio,
             valor_nominal=valor_nominal,
             foro=foro,
-            data_publicacao=data_publicacao_date
+            data_publicacao=data_publicacao_date if data_publicacao else None
         )
 
         if not precatory:
