541222364163255b80fa24ac4855e4349cb4cff8
Klayton COD.ias <65176457+Ton-Chyod-s@users.noreply.github.com>
2025-05-17 17:53:59 -0300

    doc: att

--- src/api_main/main.py
+++ src/api_main/main.py
@@ -1,3 +1,8 @@
+import sys
+import os
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
+
 from flask import Flask, render_template, session
 from dotenv import load_dotenv
 from flask_jwt_extended import JWTManager
