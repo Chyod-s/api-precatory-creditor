738da50e6a39f5d2119e62c24879eec1913a0154
Ton-Chyod-s <klayton.dias@hotmail.com>
2026-04-17 18:58:20 -0400

    feat: remover importações não utilizadas e atualizar __all__ no módulo models

--- src/api_main/infraestructure/database/base.py
+++ src/api_main/infraestructure/database/base.py
@@ -3,5 +3,9 @@ from sqlalchemy.ext.declarative import declarative_base
 Base = declarative_base()
 
 def init_db(engine):
-    from src.api_main.domain.models import Base
-    Base.metadata.create_all(bind=engine)   
+    from src.api_main.domain.models.creditor_model import Creditor
+    from src.api_main.domain.models.precatory_model import Precatory
+    from src.api_main.domain.models.personal_document_model import PersonalDocument
+    from src.api_main.domain.models.certificate_model import Certificate
+    from src.api_main.domain.models.users_model import User
+    Base.metadata.create_all(bind=engine)
